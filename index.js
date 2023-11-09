53ca41cb6ed38ef0ab8dc602e8f12324dbdafa7f
Tayler <taylerblevins@gmail.com>
2023-11-08 23:18:53 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+const num1 = 2
+const num2 = 31
+const multiply = num1 * num2;
+const random = Number.parseInt('25',10);
+const num3 = 16
+const num4 = 4
+const mod = Math.round(num3/num4);
+const max = Math.max(1,2,3,4,16,18,20)
